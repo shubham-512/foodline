d97d31398b8a5684e579a214b733e4de96293206
Shubham <kumarshubham512@gmail.com>
2021-05-01 18:22:32 +0530

    change setting

--- backend/settings.py
+++ backend/settings.py
@@ -171,10 +171,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-
-
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR,'static')
+    BASE_DIR / 'static',
+    BASE_DIR / 'frontend/build/static'
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 CORS_ALLOW_ALL_ORIGINS = True
